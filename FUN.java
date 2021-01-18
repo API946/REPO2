6c50394d6ef0a53f2785a7be625557e40deec932
API964 <aditya.veer@apisero.com>
2021-01-18 22:00:06 +0530

    Hello

---
+++ FUN.java
@@ -0,0 +1,24 @@
+class FUN
+{
+
+	public static void main(String[] args){
+
+		System.out.println("I am Aditya ");
+
+		System.out.println(" Veer");
+		
+		System.out.println(" I live in satara ");
+		
+		
+		System.out.println(" I live in pune ");
+
+		System.out.println(" I am from a branch ");
+
+		System.out.println(" I am from a MITAOE");
+
+		System.out.println(" I am from a VEER");
+
+
+	}
+
+}
